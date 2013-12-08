c11b612fd352aaffbe8cdfd071f975a8b8365985
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-12-08 18:23:12 +0000

    Remove responsibility of diameter calc from Gear.

--- gear.rb
+++ gear.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = Wheel.new(rim, tire)
   end
 
   def ratio
@@ -12,10 +11,16 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 puts Gear.new(52, 11, 24, 1.25).gear_inches
-puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11, 24, 1.25).ratio
