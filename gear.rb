d3d7fee00da5e04ca52a32d41018eed7a4cedee8
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-12-08 18:48:38 +0000

    Gear has got no Wheel responsibility any more.

--- gear.rb
+++ gear.rb
@@ -1,9 +1,10 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, rim, tire)
+
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -13,14 +14,21 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
+end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
-puts Gear.new(52, 11, 24, 1.25).ratio
+@wheel = Wheel.new(26, 1.5)
+puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(52, 11).ratio
